5ff6dd158a30ef7a6b53d6cc2a1d7f7f76e7e167
Brenton Holiday <8rents@gmail.com>
2024-03-19 18:01:54 -0700

    changed to try out oh my posh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
-Invoke-Expression (&starship init powershell)
+# Use the Starship Prompt
+# Invoke-Expression (&starship init powershell)
+
+# Use Oh My Posh Prompt
+oh-my-posh init pwsh | Invoke-Expression
 
 neofetch
