0522953d0c8caebb26d747c66c7051ce746ca5f4
Brenton Holiday <8rents@gmail.com>
2024-04-10 19:35:12 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,14 @@
+# Brent's PowerShell Profile
+# version: 0.1
+# modified: Wednesday, April 10, 2024 4:57:33 PM
+
+
 # Use the Starship Prompt
 # Invoke-Expression (&starship init powershell)
 
 # Use Oh My Posh Prompt
-oh-my-posh init pwsh | Invoke-Expression
+# oh-my-posh init pwsh | Invoke-Expression
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
 
 neofetch
