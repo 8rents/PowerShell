da94f514131fe4add3e035bca83f5b36cd880624
Brenton Holiday <8rents@gmail.com>
2025-04-09 14:10:09 -0700

    Update Microsoft.PowerShell_profile.ps1
    - added eza aliases with parameters
      - three basic list types
         - with and without hidden files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # Brent's PowerShell Profile
 # version: 0.1
-# modified: Wednesday, April 10, 2024 4:57:33 PM
-
+# previously modified: Wednesday, April 10, 2024 4:57:33 PM
+# last modified: Wednesday, April 9, 2025 1:47:33 PM
 
 # Use the Starship Prompt
 # Invoke-Expression (&starship init powershell)
@@ -9,6 +9,23 @@
 # Use Oh My Posh Prompt
 oh-my-posh init pwsh | Invoke-Expression
 
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
+# Basic Lists (No hidden files)
+function ezal { eza.exe @args '--icons' '-1' '--no-quotes' '-F'}
+Set-Alias -Name l -Value ezal
 
-Set-Alias -Name l -Value eza
+function ezall { eza.exe @args '--icons' '-l' '--git' '--no-permissions' '--no-quotes' '-h' '-F'}
+Set-Alias -Name ll -Value ezall
+
+function ezalll { eza.exe @args '--icons' '-l' '--git' '--no-quotes' '-h' '-F'}
+Set-Alias -Name lll -Value ezalll
+
+# Basic Lists (With hidden files)
+
+function ezala { eza.exe @args '--icons' '-1' '-A' '--no-quotes' '-F'}
+Set-Alias -Name la -Value ezala
+
+function ezalla { eza.exe @args '--icons' '-l' '-A' '--git' '--no-permissions' '--no-quotes' '-h' '-F'}
+Set-Alias -Name lla -Value ezalla
+
+function ezallla { eza.exe @args '--icons' '-A' '-l' '--git' '--no-quotes' '-h' '-F'}
+Set-Alias -Name llla -Value ezallla
